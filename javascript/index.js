aa20554e70cb76b99c6b539dc1bd99759875c291
zeeshan691 <rehanali69169@gmailcom>
2019-01-20 14:11:25 +0500

    navber complete

--- javascript/index.js
+++ javascript/index.js
@@ -10,3 +10,18 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+// JavaScript Document
+
+//chnage navbar view to list
+//function showListNav() {
+//    "use strict";
+//    var nav = document.getElementById("mynavbar");
+//    if (nav.className === "navbar" || nav.className === "navbar sticky") {
+//        //        nav.className += " nav-list";
+//        nav.classList.add("nav-list");
+//    } else {
+//        //        nav.className = "navbar";
+//        nav.classList.remove("nav-list");
+//    }
+//}
