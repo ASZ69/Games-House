bc3a2be77117ef8d2e35e6075940bced15009fd0
zeeshan691 <rehanali69169@gmailcom>
2019-01-20 13:15:58 +0500

    navbar and main image add

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,12 @@
-//chnage navbar view to list
-function showListNav() {
-    "use strict";
-    var nav = document.getElementById("myNavbar");
-    if (nav.className === "navbar" || nav.className === "navbar sticky") {
-        //        nav.className += " nav-list";
-        nav.classList.add("nav-list");
-    } else {
-        //        nav.className = "navbar";
-        nav.classList.remove("nav-list");
-    }
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
