71659363632d5fd53983581d0fd92cd8c4f2e777
zeeshan691 <rehanali69169@gmailcom>
2019-01-24 04:41:45 +0500

    95 % complete home page js is reming....

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
-	$('#video').append('<video id="vedio" loop><source src="../vedio/vedio.mp4" type="video/mp4"></video>');
+	$('#video').append('<video id="vedio" loop autoplay><source src="../vedio/vedio.mp4" type="video/mp4"></video>');
 	$('#vedio').get(0).play();
 })
+//window.screen = ScrollReveal();
+//screen.reveal('.container-fluid',{duration:1000});
+//screen.reveal('.container-fluid',{duration:1000});
+//screen.reveal('.row',{duration:1000});
+//screen.reveal('.cols3',{duration:1000});
