b9d3b34f9319853e3aeffd6e3785ddc41fa95d3c
zeeshan691 <rehanali69169@gmailcom>
2019-01-24 12:20:18 +0500

    home page working...

--- javascript/index.js
+++ javascript/index.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 	$('#video').append('<video id="vedio" loop autoplay><source src="../vedio/vedio.mp4" type="video/mp4"></video>');
 	$('#vedio').get(0).play();
 })
-//window.screen = ScrollReveal();
-//screen.reveal('.container-fluid',{duration:1000});
-//screen.reveal('.container-fluid',{duration:1000});
-//screen.reveal('.row',{duration:1000});
-//screen.reveal('.cols3',{duration:1000});
+window.screen = ScrollReveal();
+screen.reveal('.container-fluid',{duration:1000});
+screen.reveal('.container-fluid',{duration:1000});
+screen.reveal('.row',{duration:1000});
+screen.reveal('.cols3',{duration:1000});
