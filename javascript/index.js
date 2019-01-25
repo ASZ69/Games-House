68dc117588a9895a378c4a78830cb2070e8015cd
zeeshan691 <rehanali69169@gmailcom>
2019-01-25 16:15:38 +0500

    now complete home page ...

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,29 @@ $(document).ready(function(){
 })
 
 ScrollReveal().reveal('.scroll-up',{duration:3000});
-//
-//var body = document.getElementsByTagName('body')[0];
-//var tag = body.parentElement.nodeName;
-//alert('tag');
+
+function btnfol(ref){
+//	alert(btncol);
+	var btncol = document.getElementsByClassName('btncol')[0];
+	btncol.innerHTML = 'Thanks To Follow me';
+	btncol.style.fontSize = '1.8vw';
+	btncol.style.color = 'white';
+    var pos = 0;
+    var colid = setInterval(frame, 5);
+	
+    function frame() {
+    if (pos == 30) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      btncol.style.top = pos + "px"; 
+      btncol.style.bottom = pos + "px"; 
+    }
+  }
+}
+
+function btnread(btnread){
+	alert(navabout);
+	var navabout = document.getElementsByClassName('nav-about')[0];
+	navabout.location.href = 'about-us.html';
+}
