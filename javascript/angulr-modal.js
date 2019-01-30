3ac848a52d044d30e375ba525cde8b3de94d5224
zeeshan691 <rehanali69169@gmailcom>
2019-01-30 23:48:13 +0500

    stric ready left block ready right pendding...

---
+++ javascript/angulr-modal.js
@@ -0,0 +1,209 @@
+
+var games = angular.module("games",[]);
+
+games.controller("cat-ctrl",function($scope,$filter){
+	$scope.showCat = function(e){
+		var ref = e.currentTarget;
+		var cat = ref.innerHTML;
+		
+		$scope.evearray = $filter('filter')(itemlist, {name: cat});
+	};
+	
+	$scope.evearray = $filter('filter')(itemlist, {name: 'Action games'});
+});
+
+
+var itemlist = [{name:'Action games',
+				 seasons:[
+					 
+					 {name:'FarCry',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						 videos:'games.mp4',
+					                 }},
+					 
+					 {name:'Max Payne 3',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						   videos:'games.mp4'}},
+					 
+					 {name:'Grand Theft Auto: San Andr...',
+					 requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}},
+					 
+					  {name:'Grand Theft Auto: Vice city',
+					   requirements:'www.wiki.com/psl', 
+					   imgSrc:'../images/myimg.jpg',
+					    moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}}
+				 ]},
+				
+				{name:'Shooting games',
+				 seasons:[
+					 
+					 {name:'CallofDuty',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}},
+					 
+					 {name:'Battlefield',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}},
+					 
+					 {name:'SniperElite',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}},
+					 
+					 {name:'FallOut',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}}
+				 ]},
+				
+				{name:'Sport games',
+				 seasons:[
+					 
+					 {name:'Circket07',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}},
+					 
+					 {name:'FIFA 18',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}},
+					 
+					 {name:'NBA Playground',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}},
+					
+				 ]},
+				
+				{name:'Puzzle games',
+				 seasons:[
+					 
+					 {name:'Jigsaw Puzzles Free',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}},
+					 
+					 {name:'The Witness',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}},
+					 
+					 {name:'Magic Jigsaw Puzzles',
+					  requirements:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  videos:'games.mp4'}},
+					
+				 ]}
+				
+				
+			   ];
+
+// var dataList = [
+//		{heading:'FarCry',season:'2004',
+//		 requirements:'bdfddg',
+//		 imgSrc:'../images/myimg.jpg',
+//		 moreDetails:{
+//			 images:{
+//				 image1:'../images/my.jpg',
+//			 	 image2:'asdasda',
+//			 	 image3:'adsfda'}}, 
+//		{heading:'The PSL',season:'2017',
+//			 wiki:'www.wiki.com/psl',
+//			 imgSrc:'../images/myimg.jpg',
+//			 moreDetails:{
+//				 images:{
+//					 image1:'../images/my.jpg',
+//					 image2:'asdasda',
+//					 image3:'adsfda'},
+//				 scoreDefeat:'24/7',
+//				 scoreWin:'12/24',
+//				 matchHost:'landhi'}},
+//		{heading:'The ICC',season:'2017',
+//		 wiki:'www.wiki.com/psl17',
+//		 imgSrc:'../images/myimg17.jpg',
+//		 moreDetails:{
+//			 images:{
+//				 image1:'../images/myfor3.jpg',
+//			 	 image2:'axzxz',
+//			 	 image3:'iii'},
+//			 scoreDefeat:'69/7',
+//			 scoreWin:'1/240',
+//		 	 matchHost:'nadi kinare'}}
+//		];
