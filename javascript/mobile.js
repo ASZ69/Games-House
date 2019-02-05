98af2064d4d83edcbddd883c313bf8c3ab2496db
zeeshan691 <rehanali69169@gmailcom>
2019-02-06 00:57:40 +0500

    mobile page complite

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -1,1 +1,532 @@
-// JavaScript Document
+	
+var games = angular.module("games", []);
+
+games.controller("cat-ctrl", function ($scope, $filter) {
+    $scope.showCat = function (e) {
+        var ref = e.currentTarget;
+        var cat = ref.innerHTML;
+
+        $scope.evearray = $filter('filter')(itemlist, { name: cat });
+        $(".right-view").css("display", "flex");
+        $(".right-page").css("display", "none");
+
+        hoverSetting();
+
+    };
+
+    $scope.evearray = $filter('filter')(itemlist, { name: 'Arcade games' });
+
+    $scope.viewMore = function (game) {
+        var clikeRf = game.currentTarget;
+        var gameName = clikeRf.parentElement.parentElement.parentElement.getElementsByTagName("h3")[0].innerHTML;
+
+
+        $(".right-view").css("display", "none");
+        $(".right-page").css("display", "block");
+
+        $scope.moredetailsarray = $filter('filter')($scope.evearray[0].seasons, { name: gameName });
+
+
+    };
+
+    $scope.viewGame = function (games) {
+        var clickref = games.currentTarget;
+        var gameName = clickref.innerHTML;
+
+        var gameCat = clickref.parentElement.parentElement.getElementsByTagName("h3")[0].innerHTML;
+        $scope.evearray = $filter('filter')(itemlist, { name: gameCat });
+
+        $scope.moredetailsarray = $filter('filter')($scope.evearray[0].seasons, { name: gameName });
+
+        setTimeout(function () {
+            $(".right-view").css("display", "none");
+            $(".right-page").css("display", "block");
+        }, 70);
+
+        scrollUp();
+        hoverSetting();
+
+    };
+
+    $scope.evearray = $filter('filter')(itemlist, { name: 'Arcade games' });
+
+    $scope.catarray = itemlist;
+    hoverSetting();
+
+});
+
+function scrollUp() {
+    $(window).scrollTop(0);
+}
+
+function hoverSetting() {
+    setTimeout(function () {
+        $('.card-overlay a').on("mouseover", function () {
+            //		alert("hello");
+            $(this).parent().parent().find("img").eq(0).addClass("image-hover");
+        });
+
+        $('.card-overlay a').on("mouseleave", function () {
+            //		alert("hello");
+            $(this).parent().parent().find("img").eq(0).removeClass("image-hover");
+        });
+
+    }, 1000);
+}
+
+var itemlist = [{
+    name: 'Arcade games',
+    seasons: [
+
+        {
+            name: 'Fruit Ninja',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/arcade-games/fruitninja.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/F-1.jpg',
+                    '../mobile-games/screenshots/F-2.jpg',
+                    '../mobile-games/screenshots/F-3.jpg',
+                    '../mobile-games/screenshots/F-4.jpg',
+                ],
+                releaseDate: '2010',
+                updatedOn: '11 jun, 2010',
+                genre: 'Arcade',
+                category: 'Arcade',
+                views: '11290',
+                votes: '69',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Tample Run',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/arcade-games/temple-run.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/t-1.jpg',
+                    '../mobile-games/screenshots/t-2.jpg',
+                    '../mobile-games/screenshots/t-3.jpg',
+                    '../mobile-games/screenshots/t-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Subway surfers',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/arcade-games/subway.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/sw-1.jpg',
+                    '../mobile-games/screenshots/sw-2.jpg',
+                    '../mobile-games/screenshots/sw-3.jpg',
+                    '../mobile-games/screenshots/sw-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Angry Birds',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/arcade-games/angrybirds.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/ab-1.jpg',
+                    '../mobile-games/screenshots/ab-2.jpg',
+                    '../mobile-games/screenshots/ab-3.jpg',
+                    '../mobile-games/screenshots/ab-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Angry Birds 2',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/arcade-games/angrybirds 2.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/ab2-1.jpg',
+                    '../mobile-games/screenshots/ab2-2.jpg',
+                    '../mobile-games/screenshots/ab2-3.jpg',
+                    '../mobile-games/screenshots/ab2-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+    ]
+},
+
+
+{
+    name: 'Racing &amp; flying',
+    seasons: [
+
+        {
+            name: 'Asphalt8: Airborne',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/racing-game/asphalts.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/as-1.jpg',
+                    '../mobile-games/screenshots/as-2.jpg',
+                    '../mobile-games/screenshots/as-3.jpg',
+                    '../mobile-games/screenshots/as-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Traffic Racer',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/racing-game/traffic-racer.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/tr-1.jpg',
+                    '../mobile-games/screenshots/tr-2.jpg',
+                    '../mobile-games/screenshots/tr-3.jpg',
+                    '../mobile-games/screenshots/tr-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Traffic Rider',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/racing-game/traffic-rider.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/trf-1.jpg',
+                    '../mobile-games/screenshots/trf-2.jpg',
+                    '../mobile-games/screenshots/trf-3.jpg',
+                    '../mobile-games/screenshots/trf-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Apocalypse',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/racing-game/apolo.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/apo-1.jpg',
+                    '../mobile-games/screenshots/apo-2.jpg',
+                    '../mobile-games/screenshots/apo-3.jpg',
+                    '../mobile-games/screenshots/apo-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Inosane',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/racing-game/inosane.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/ino-1.jpg',
+                    '../mobile-games/screenshots/ino-2.jpg',
+                    '../mobile-games/screenshots/ino-3.jpg',
+                    '../mobile-games/screenshots/ino-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Sega game',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/racing-game/sega-game.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/se-1.jpg',
+                    '../mobile-games/screenshots/se-2.jpg',
+                    '../mobile-games/screenshots/se-3.jpg',
+                    '../mobile-games/screenshots/se-1.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+    ]
+},
+
+{
+    name: 'Fighting games',
+    seasons: [
+
+        {
+            name: 'Shadow Fight 2',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/fighting-game/shedow 2.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/she-1.jpg',
+                    '../mobile-games/screenshots/she-2.jpg',
+                    '../mobile-games/screenshots/she-3.jpg',
+                    '../mobile-games/screenshots/she-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290', 
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Shadow Fight 3',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/fighting-game/shedow 3.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/shew-1.jpg',
+                    '../mobile-games/screenshots/shew-2.jpg',
+                    '../mobile-games/screenshots/shew-3.jpg',
+                    '../mobile-games/screenshots/shew-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+            }
+        },
+
+
+        {
+            name: 'Stickman Legends',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/fighting-game/stickman.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/stick.jpg',
+                    '../mobile-games/screenshots/stick-2.jpg',
+                    '../mobile-games/screenshots/stick-3.jpg',
+                    '../mobile-games/screenshots/stick-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+
+        {
+            name: 'XenoShyft',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/fighting-game/x.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/x-1.jpg',
+                    '../mobile-games/fighting-game/x.jpg',
+                    '../mobile-games/screenshots/x-1.jpg',
+                    '../mobile-games/fighting-game/x.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+
+        {
+            name: 'Soul Calibur 4',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/fighting-game/soul-caliber.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/screenshots/soul-1.jpg',
+                    '../mobile-games/screenshots/soul-2.jpg',
+                    '../mobile-games/screenshots/soul-3.jpg',
+                    '../mobile-games/screenshots/soul-4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+
+
+
+    ]
+},
+
+{
+    name: 'Strategy games',
+    seasons: [
+
+        {
+            name: 'Rool of galaxy',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/strategy-games/galaxy.jpg',
+            requirements: {
+                images: [
+                    '../windows-games/screen-shots/puzzle-game/1.jpg',
+                    '../mobile-games/strategy-games/galaxy.jpg',
+                    '../windows-games/screen-shots/puzzle-game/3.jpg',
+                    '../windows-games/screen-shots/puzzle-game/4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+                
+            }
+        },
+
+        {
+            name: 'RadIand Historia',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/strategy-games/red.jpg',
+            requirements: {
+                images: [
+                    '../windows-games/screen-shots/puzzle-game/1.jpg',
+                    '../windows-games/screen-shots/puzzle-game/2.jpg',
+                    '../windows-games/screen-shots/puzzle-game/3.jpg',
+                    '../mobile-games/strategy-games/red.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Shadow Guardian',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/strategy-games/shed.jpg',
+            requirements: {
+                images: [
+                    '../windows-games/screen-shots/puzzle-game/1.jpg',
+                    '../mobile-games/strategy-games/shed.jpg',
+                    '../windows-games/screen-shots/puzzle-game/3.jpg',
+                    '../windows-games/screen-shots/puzzle-game/4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+        {
+            name: 'Strike Tactics',
+            download: 'http://oceanoffgames.com/category/home/',
+            imgSrc: '../mobile-games/strategy-games/s.jpg',
+            requirements: {
+                images: [
+                    '../mobile-games/strategy-games/s-1.jpg',
+                    '../windows-games/screen-shots/puzzle-game/2.jpg',
+                    '../mobile-games/strategy-games/s-2.jpg',
+                    '../windows-games/screen-shots/puzzle-game/4.jpg',
+                ],
+                releaseDate: '2004',
+                updatedOn: '23 March, 2004',
+                genre: 'Action',
+                category: 'Action',
+                views: '11290',
+                votes: 'N/A',
+                officialSite: 'http://oceanoffgames.com/category/home/',
+            }
+        },
+
+    ]
+}
+
+
+];
